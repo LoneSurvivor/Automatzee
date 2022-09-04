0daaea014145b3f3d565731e90ca605004335710
Nitin <nitinbhardwaj007@gmail.com>
2022-09-04 21:48:41 +0530

    Updates

---
+++ precache-manifest.585afd6d57b4cb9321f04b9ff582a37e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a54e680e3488bd9a5906",
+    "url": "/automatzee/static/css/main.ab670461.chunk.css"
+  },
+  {
+    "revision": "a54e680e3488bd9a5906",
+    "url": "/automatzee/static/js/main.a54e680e.chunk.js"
+  },
+  {
+    "revision": "d541de35f7ffe24a42bf",
+    "url": "/automatzee/static/js/1.d541de35.chunk.js"
+  },
+  {
+    "revision": "ba1a824274e4ad45d9f0",
+    "url": "/automatzee/static/css/2.8233a55e.chunk.css"
+  },
+  {
+    "revision": "ba1a824274e4ad45d9f0",
+    "url": "/automatzee/static/js/2.ba1a8242.chunk.js"
+  },
+  {
+    "revision": "74c6c5c4fd9680b8d883",
+    "url": "/automatzee/static/js/3.74c6c5c4.chunk.js"
+  },
+  {
+    "revision": "a2baf2c3c4834f090421",
+    "url": "/automatzee/static/js/4.a2baf2c3.chunk.js"
+  },
+  {
+    "revision": "50a74afdc3bcec516984",
+    "url": "/automatzee/static/js/5.50a74afd.chunk.js"
+  },
+  {
+    "revision": "c9a13ea5eab3977f9bf8",
+    "url": "/automatzee/static/js/runtime~main.c9a13ea5.js"
+  },
+  {
+    "revision": "9e54924fe54f9b118fb0066ed90353e2",
+    "url": "/automatzee/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/automatzee/precache-manifest.585afd6d57b4cb9321f04b9ff582a37e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/automatzee/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
